27e09a9100fd941c53299a16f990f5d9951e94fb
GOPIKRISHNAN P V <gopikrishnanpv9@gmail.com>
2023-11-26 13:53:01 +0530

    restructuring the files

--- FarmMate/home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def loadHome(request,user):
-    
-    return render(request,'home/home.html')
--- FarmMate/manage_account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-urlpatterns=[
-    path('login/',loginForm,name='userlogin'),
-    path('registration/',registrationForm,name='signup'),
-    path('',home,name='home'),
-    path('logout/', logoutUser, name='logout')
-]
