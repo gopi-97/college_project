b9f48b5a15b9df4af6ca225bba4fd69fd614c680
GOPIKRISHNAN P V <gopikrishnanpv9@gmail.com>
2023-11-30 10:53:33 +0530

    updated urls

--- FARMMATE/FarmMate/urls.py
+++ FARMMATE/FarmMate/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("manage_account.urls")),
-
+    path("",include("dashboard.urls")),
 ]
