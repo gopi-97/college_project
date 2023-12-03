84a67753ca2f745445b0ad41c8343e6aa8c56e4b
GOPIKRISHNAN P V <gopikrishnanpv9@gmail.com>
2023-12-03 22:53:40 +0530

    updated views.py

--- FARMMATE/dashboard/views.py
+++ FARMMATE/dashboard/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
-
+from manage_account.models import Client
+from .form import addProductForm
+from .models import Cultivation
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
+@login_required
+def loadHome(request,user):
+    return render(request,'dashboard/home.html')
+
+
+def add_product(request):
+    if request.method=="POST":
+        try:
+            form=addProductForm
+            if form.is_valid():
+                Cultivation=form.save(commit=True)
+            else:
+               return render(request,'dashboard/dashboard.html')
+        except Exception as e:
+            print(e)
+
+
+
 
 def load_dash(request):
     return render(request,'dashboard/dashboard.html')
