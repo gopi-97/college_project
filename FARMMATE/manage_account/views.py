bda4154bbab74ed7655d1d031ce0a9b37d7304a9
GOPIKRISHNAN P V <gopikrishnanpv9@gmail.com>
2024-01-02 20:51:34 +0530

    major changes to several views

--- FARMMATE/manage_account/views.py
+++ FARMMATE/manage_account/views.py
@@ -1,90 +1,75 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
-from .forms import ClientRegi as cr, ClientLogin as cl
+from .forms import FarmerLoginForm,FarmerRegistrationForm
 from django.contrib.auth import  login, logout
-from .backends import ClientBackend
 from django.core.mail import send_mail,BadHeaderError
 from django.contrib import messages
-from .models import Client
-import random
-import string
-
+from .models import Farmer
+from .custom_exceptions import USERNAMEEXISTS
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+from django.contrib.auth import authenticate
+from django.contrib.auth.views import LoginView ,LogoutView
 
-def home(request):
-    return render(request, 'dashboard/home.html')
+class home(LoginRequiredMixin,TemplateView):
+    template_name='dashboard/home.html'
 
-#userid generator
-def generate_userid():
-    chars = string.ascii_letters + string.digits
-    id = "".join(random.choice(chars) for _ in range(10)) #generator function
-    return id
 
-# user registration form
-def registrationForm(request):
-    if request.method == 'POST':
-        form = cr(request.POST)
-        try:
-            if form.is_valid():
-                user = form.save(commit=False)
-                user.set_password(form.cleaned_data['password'])
-                
-                # this line creates a list of all the userids in the client model
-                existing_ids = [user.userid for user in Client.objects.all()]
-        
-                unique_id = generate_userid()
-                while True:
-                
-                    if unique_id not in existing_ids:
-                        user.userid = unique_id
-                        break
-                    else:
-                        unique_id = generate_userid()
-                
-                user.save()
-                user = Client.objects.get(userid=unique_id)
-                new_username = user.username + ''.join(random.choice(string.digits) for _ in range(5))
-                user.username = new_username
-                user.save()
-                # Send email
-                # subject = 'Your User ID'
-                # message = f'Your User ID is: {unique_id}'
-                # from_email = EMAIL_HOST_USER
-                # recipient_list = [user.email]
-                # print(from_email)
-
-                # try:
-                #     send_mail(subject, message, from_email, recipient_list)
-                # except BadHeaderError as b:
-                #     print(b)
-
-                messages.success(request, 'Registration successful. You can now login.')
-                return redirect('userlogin')
-            
-        except Exception as e:
-           print(e)
-    else:
-        form = cr()
-
-    return render(request, 'manage_account/registration.html', {'form': form})
-
-# modify this function to generate precise error feedback
 def generate_message(request,error):
     # Iterate through stored messages and discard them
         for message in messages.get_messages(request):
            pass
         messages.error(request,error)
+    
+# user registration view
 
-def loginForm(request):
+class register(FormView):
+    model=Farmer
+    form_class=FarmerRegistrationForm
+    template_name='manage_account/registration.html'
+    redirect_authenticated_user=True
+    success_url=reverse_lazy('userlogin')
+
+    def form_valid(self, form: FarmerRegistrationForm) -> HttpResponse:
+        password = form.cleaned_data.get('password1')
+        confirm_password = form.cleaned_data.get('password2')
+
+        if password != confirm_password:
+            messages.error(self.request, "Passwords don't match.")
+            return self.form_invalid(form)
+
+        try:
+            form.save()
+            messages.success(self.request, "Account created successfully!")  # Display success message
+        except USERNAMEEXISTS as e:  # Handle specific exceptions
+            messages.error(self.request, f"Error: {e}")  # Display specific error message
+        except Exception as e:  # Catch other exceptions
+            messages.error(self.request, f"Unexpected error: {e}")
+
+        return super().form_valid(form)
+    
+    def get(self,*args,**kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('userlogin')
+        return super(register,self).get(*args,**kwargs)
+
+
+def userLogin(request):
     try:
        
         if request.method == 'POST':
-            userid = request.POST['userid']
+            username = request.POST['username']
             password = request.POST['password']
             #calling the custom backend function
-            user=ClientBackend().authenticate(username=userid, password=password)
+            user=authenticate(username=username, password=password)
             if user is not None:
                 # Specify the backend explicitly when calling login() as we are using custom authentication backend 
-                login(request, user,backend='manage_account.backends.ClientBackend')
+                login(request, user)
+                request.session['username'] =username
                 messages.success(request, 'Login successful.')
                 return redirect('home') 
             else:
@@ -94,9 +79,9 @@ def loginForm(request):
     except Exception as e:
         generate_message(request,e)
 
-    return render(request, 'manage_account/login.html', {'form': cl()})
+    return render(request, 'manage_account/login.html', {'form': FarmerLoginForm()})
 
-def logoutUser(request):
-    request.session.flush()
-    logout(request)
-    return redirect('userlogin')
+
+class UserLogout(LogoutView) :
+    next_page = reverse_lazy('userlogin')
+
