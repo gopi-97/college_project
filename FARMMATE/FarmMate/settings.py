af7014ff4869a55164b92d7f30c734daa8349dce
GOPIKRISHNAN P V <gopikrishnanpv9@gmail.com>
2024-01-02 20:36:44 +0530

    major changes

--- FARMMATE/FarmMate/settings.py
+++ FARMMATE/FarmMate/settings.py
@@ -13,15 +13,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import django_bootstrap5
 import os
+from dotenv import load_dotenv
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# Load environment variables from .env file
+dotenv_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path)
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY',)
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -45,6 +53,10 @@ EXTERNAL_APPS=[
     'manage_account',
     'django_bootstrap5', 
     'dashboard',
+    'todo',
+    'django_extensions',
+    'django_htmx',
+    "template_partials",
 ]
 
 INSTALLED_APPS+=EXTERNAL_APPS
@@ -57,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',#adds request.htmx attribute to request object
 ]
 
 ROOT_URLCONF = 'FarmMate.urls'
@@ -87,12 +100,8 @@ WSGI_APPLICATION = 'FarmMate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'FarmMate_DB',
-        'USER': 'root',       
-        'PASSWORD': 'mysql',  
-        'HOST': 'localhost',             
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -115,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTH_USER_MODEL='manage_account.Farmer'
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -133,7 +142,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS =[ 
-    os.path.join(BASE_DIR, 'static' ) #static folder inside the project for every app
+    os.path.join(BASE_DIR) #static folder inside the project for every app
     ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -141,12 +150,11 @@ STATICFILES_DIRS =[
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Authentication backed which uses function from backends.py
-AUTHENTICATION_BACKENDS = [
-    'manage_account.backends.ClientBackend',  
+AUTHENTICATION_BACKENDS = [  
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-
+LOGIN_URL='userlogin'
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # or any other session engine
 SESSION_COOKIE_SECURE = False  # Set to True if using HTTPS
